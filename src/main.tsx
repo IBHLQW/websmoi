4c88e5e12389499372c55014022cbf669e6d01c2
IBHLQW <personalusage2009@gmail.com>
2026-03-17 10:25:24 +0700

    deploy-debug2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
-import './src/style.css';
+import App from './App';
+import './index.css';
+
+console.log('Datalyse main.tsx loaded');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
