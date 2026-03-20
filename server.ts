b65ad39c06310eba64a1eddb791316a0ebf96cc0
IBHLQW <personalusage2009@gmail.com>
2026-03-20 17:17:43 +0700

    deploy-debug-final

---
+++ server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from "express"
+import { createServer as createViteServer } from "vite";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function startServer() {
+  const app = express();
+  const PORT = 3000;
+
+  app.use((req, res, next) => {
+    if (req.url.endsWith('.ts') || req.url.endsWith('.tsx')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+    next();
+  });
+
+
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: "spa",
+  });
+  app.use(vite.middlewares);
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer().catch((err) => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
+});
