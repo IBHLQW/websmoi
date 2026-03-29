4834c96d1d61c9126b1569ecfa20607e21aab7f4
IBHLQW <personalusage2009@gmail.com>
2026-03-30 01:48:49 +0700

    .

--- vite.config.tsx
+++ vite.config.tsx
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/websmoi/', // Update this to match your repository name exactly
+  base: './', // Use relative paths to ensure assets load correctly on GitHub Pages
   plugins: [react(), tailwindcss()],
 });
