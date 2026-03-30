e61b68113eb9f2c13f0a0ce13b7d534d9dec01ba
IBHLQW <personalusage2009@gmail.com>
2026-03-30 09:03:25 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './style.css';
 
-// Cache-busting deploy: 2026-03-29-18-58
-console.log('Datalyse main.tsx loaded - Version 1.1.2');
+// Cache-busting deploy: 2026-03-29-19-02
+console.log('Datalyse main.tsx loaded - Version 1.1.3');
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
